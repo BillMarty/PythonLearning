b49b9bda312202b96d8b23bf71f337addbe25fdb
BillMarty <bmarty@planetarypower.com>
2016-09-19 14:38:51 -0700

    Trying again to change the encoding of dicty_stuff.py.

--- dicty_stuff.py
+++ dicty_stuff.py
@@ -5,13 +5,13 @@
 # How about info from my contacts list?  A list of dictionaries, one per person.
 cb_contacts = []
 # Last Name,First Name,Organization,Phone : Mobile,Phone : Home,Phone : iPhone,Email : Home,Email : Other,Email : Work,Address : Home : Street,Address : Home : City,Address : Home : State,Address : Home : Country,Address : Home : ZIP,URL : HomePage,Note
-# ,Okoth,Fr. Crispin,,(206) 794-7487,,,frcrispin@stjohnsea.org,,,,,,,,,
-cb_contacts.append({'Last Name': 'Okoth', 'First Name': 'Fr. Crispin', 'Organization': '', 'Phone': '(206) 794-7487', 'Email': 'frcrispin@stjohnsea.org'})
+# ,Okoth,Fr. Crispin,,(206)?794-7487,,,frcrispin@stjohnsea.org,,,,,,,,,
+cb_contacts.append({'Last Name': 'Okoth', 'First Name': 'Fr. Crispin', 'Organization': '', 'Phone': '(206)?794-7487', 'Email': 'frcrispin@stjohnsea.org'})
 # ,Kent,Lawrence,Gates Foundation,,,,,lawrence.kent@gatesfoundation.org,,,,,,,,
 cb_contacts.append({'Last Name': 'Kent', 'First Name': 'Lawrence', 'Organization': 'Gates Foundation', 'Phone': '', 'Email': 'lawrence.kent@gatesfoundation.org'})
-# ,Beberness,Soheila,,1 (503) 381-1122,,,soheilab@live.com,,,9013 Forest Hill Pl NW,Seattle,WA,,98177,,
-cb_contacts.append({'Last Name': 'Beberness', 'First Name': 'Soheila', 'Organization': '', 'Phone': '1 (503) 381-1122', 'Email': 'soheilab@live.com'})
-# ,Kent,Kristin,,,(206) 783-1949,(206) 852-0393,,dearkristink@yahoo.com,,,,,,,,
+# ,Beberness,Soheila,,1?(503)?381-1122,,,soheilab@live.com,,,9013 Forest Hill Pl NW,Seattle,WA,,98177,,
+cb_contacts.append({'Last Name': 'Beberness', 'First Name': 'Soheila', 'Organization': '', 'Phone': '1?(503)?381-1122', 'Email': 'soheilab@live.com'})
+# ,Kent,Kristin,,,(206) 783-1949,(206)?852-0393,,dearkristink@yahoo.com,,,,,,,,
 cb_contacts.append({'Last Name': 'Kent', 'First Name': 'Kristin', 'Organization': '', 'Phone': '(206) 783-1949', 'Email': 'dearkristink@yahoo.com'})
 # ,Marty,Sheila,,,206 794-0992,,,,smarty@stjohnsea.org,334 N 83rd Street,Seattle,WA,United States,98103,,
 cb_contacts.append({'Last Name': 'Marty', 'First Name': 'Sheila', 'Organization': '', 'Phone': '206 794-0992', 'Email': 'smarty@stjohnsea.org'})

a1e5a2ee93b3f35ad6f307b25fda7e02c3da47bc
BillMarty <bmarty@planetarypower.com>
2016-09-20 14:24:09 -0700

    Copying Matthew's adc.py. Adding my own adc_test.py.

---
+++ adc_stuff/adc_test.py
@@ -0,0 +1,21 @@
+# Bill Marty's code to test Matthew West's adc.py
+
+import adc
+import time
+
+#Which pin are we reading?
+pin = 'P9_38'
+
+print('Calling setup().')
+setup()
+if adc_setup:
+    print('Setup returned adc_setup = True.' )
+else:
+    print('Setup returned adc_setup = False.')
+
+print('Reading P9_38:')
+for n in [10]:
+    time.sleep(0.5)
+    volts = read_volts(pin)
+    adc_count = read_raw(pin)
+    print(str(pin) + ': volts = ' + str(volts) + ', counts = ' + str(adc_count))

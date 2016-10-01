325160e36a660c30825cfbe3abc2cb795cf59908
BillMarty <bmarty@dbmedx.com>
2016-10-01 10:04:10 -0700

    Rebuilding my contacts program around classes.

---
+++ read_contacts/.idea/MyContacts.py
@@ -0,0 +1,32 @@
+# Python learning project:
+#   Build a Contacts management program that reads in my contacts (from a .csv file to start).
+
+contacts_file_path = '/Users/billmarty/PythonLearning/CB_contacts.csv'
+
+class MyContacts():
+    """A class to contain a list of my contacts."""
+
+    def __init__(self, in_file):
+        """init: Don't know what goes here yet."""
+        assert 'csv' in in_file, '!File is not *.csv!'
+        self.read_from_csv_file(in_file)
+
+    def read_from_csv_file(self, in_file):
+        """Import a csv file that was exported from the Apple Contacts application.
+                file: path specification for the csv file"""
+
+        with open(in_file) as file:
+            lines = file.readlines()
+            print('Read ' + str(len(lines)) + ' lines from ' + in_file)
+
+        print('The header is: ' + lines[0])
+        header = lines[0]
+        del lines[0]
+
+        #Separate out the filds in header
+        fields = str.split(header, ',')
+        print(fields)
+        print('There are ' + str(len(lines)) + ' contact records in the list.')
+
+
+my_contacts = MyContacts(contacts_file_path)

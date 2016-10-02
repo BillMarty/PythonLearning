c561d4140fd6cbb232b6df906c256785c9018bda
BillMarty <bmarty@dbmedx.com>
2016-10-01 17:07:41 -0700

    Progress handling difficult lines in the csv file. Not completely correct yet.

--- read_contacts/.idea/Contact.py
+++ read_contacts/.idea/Contact.py
@@ -4,6 +4,7 @@
 
 class Contact():
     """Contact holds one contact as a dictionary, likely multi-level dictionary."""
+    contact_info = {}
 
     def __init__(self, header, contact_data):
         """Contact constructor - parse contact_data according to header, to fill a dictionary of information."""
@@ -11,9 +12,18 @@ class Contact():
 
     def parse_csv_fields(self, header, contact_data):
         field_data = str.split(contact_data, ',')
-        print(header)
-        print(field_data)
-        print('Lengths of header, field_data: ' + str(len(header)) + ', ' + str(len(field_data)))
+        #print(header)
+        #print(field_data)
+        #print('Lengths of header, field_data: ' + str(len(header)) + ', ' + str(len(field_data)))
+
+        # Match up the lists to create a dictionary.  Include only fields with non-empty data
+        for index in range(len(header)):
+            assert len(header) == len(field_data), print('!!Length mismatch!! \n' + str(field_data))
+            if field_data[index]:
+                self.contact_info[header[index]] = field_data[index]
+
+        print(self.contact_info)
+
 
 
     def print_contact(self):
